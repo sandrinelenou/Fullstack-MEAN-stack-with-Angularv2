6a35353cffc4130061b83e89f9b79acd1c9643ac
sandrine Youmbissi <yo.sandra@yahoo.it>
2021-08-05 18:53:22 +0200

    opload file in category

--- Client/src/app/categories/form/categories-form.component.ts
+++ Client/src/app/categories/form/categories-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';  //fundamentale per le form
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Categorie } from '../../models/categorie';
@@ -15,59 +15,69 @@ export class CategoriesDataFormComponent implements OnInit {
 
   public form!: FormGroup;
   public id: any;
+  public isLoading = false;
+  public isSubmitted = false;
+  public categorie!: Categorie; 
+  public mode = "create";   //default
+  public param!: String;
   private categoryId: any;     //id from route public id!: string;
-  public isAddMode: any; //!: boolean;
-  public  isLoading = false;
-  public categorie!: Categorie;
-  private mode = 'create';   //default
   public categorieDetails: any;
+  public pageTitle!: String;
+    title: any;
+    content: any;
+  
 
 
   constructor(
      private categorieService: CategorieService,
      public route: ActivatedRoute,
-     private router: Router,
+    private router: Router,
+    private fb: FormBuilder
      //private formBuilder: FormBuilder
-   ) { }
+  ) { }
+  
 
- ngOnInit(): void {
-  let context = this;
-  switch (context.mode) {
-    case 'ADD':
-      context.createCategorie();
-      break;
-    case 'EDIT':
-      context.updateCategorie(this.id,this.categorieDetails);
-      break;
-
-  }
-  this.id = this.route.snapshot.params['categoryId'];
+  ngOnInit(): void {
+    
+    let context = this;
+    switch (this.mode) {
+      case 'ADD':
+        context.pageTitle = "Create Categorie";
+        this.createCategorie();
+        break;
+      case 'EDIT':
+        context.pageTitle = "Update categorie";
+        this.updateCategorie();
+        break;
+    }
+   //this.id = this.route.snapshot.params['categoryId'];
 
   this.form = new FormGroup({
     title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
     content: new FormControl(null, { validators: [Validators.required] })
- });
+  });
 
- this.route.paramMap.subscribe((paramMap: ParamMap) => {
-  if (paramMap.has('id')) {
-    this.mode = 'edit';
-    this.categoryId = paramMap.get('id');
-
-    this.isLoading = true;            //loading spinner initial
-    this.categorieService.getCategorie(this.id).subscribe((categorieData:any) => {
-      this.isLoading = false;
-      this.categorie = { id: categorieData.data._id, title: categorieData.data.title, content: categorieData.data.content };   //_id comming from the db
-      this.form.setValue(
-        {
-         title: this.categorie.title,
-         content: this.categorie.content
-        });         // allow us to ovveride the value form your form control when we want to update
-    });
-  } else {
-    this.mode = 'create';
-    this.categoryId = null;
-  }
-});
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      //const categoryId = +parameterMap.get('id')!;
+      if (paramMap.has('categoryId')) {
+        this.mode = "EDIT";
+        this.categorieService.getCategorie(this.categoryId).subscribe((categorieData: any) => {
+          this.categorie = {
+            id: categorieData.data._id,
+            title: categorieData.data.title,
+            content: categorieData.data.content
+          }
+          this.form.setValue({
+            title: this.categorie.title,
+            content: this.categorie.content
+          });
+        });
+      } else {
+        this.mode = "ADD";
+        this.categoryId = null;
+      }    
+              
+  });
 
 
   }
@@ -75,46 +85,64 @@ export class CategoriesDataFormComponent implements OnInit {
     return this.form.controls;
   }
 
-
- onSavePost() {
-     if (this.form.invalid) {
-       return;
+  onImagePicked(event: Event) {
+    //const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      //avatar: file
+    });
+    this.form.get('image')!.updateValueAndValidity();
   }
 
+  onSavePost() {
+    console.log(this.form.value)
+    this.isSubmitted = true;
+     if (this.form.invalid) {
+       return;
+     }
      this.isLoading = true;
-     if(this.mode === 'create'){    //if(this.mode == 'ADD'){
+    if (this.mode === "ADD") {    //if(this.mode == 'ADD'){
+      //console.log(this.form.value);
       this.createCategorie();
+      alert('Created successfully!');
+    } else {
+      console.log(this.form.value);
+      this.updateCategorie();
+    } 
+    this.form.reset();
+  }
 
-     }else{
-      this.updateCategorie(this.id, this.form.value);
-    }
-
-}
+  private getCategorie(id: string) {
+    if (this.id === 0) {
+      this.categorie = {
+        id: '',
+        title: '',
+        content: ''
+      };
+    } else {
+      let context = this;
+      context.categorieService.getCategorie(context.id).subscribe((categorieData: any) => {
+        context.categorieDetails = categorieData.data;
+        console.log(context.categorieDetails);
+      });
+    }    
+  }
 
 private createCategorie(){
-  console.log(this.form.value);
-  this.categorieService.createCategorie( this.form.value ).subscribe(res => {
+    this.categorieService.createCategorie( this.form.value ).subscribe(res => {
     console.log('Categorie created successfully!');
     this.router.navigateByUrl('/categories');
   })
   }
 
-
-private updateCategorie(id: string, categorie: Categorie){
+  private updateCategorie() {    
   console.log(this.form.value);
-  this.categorieService.updateCategorie( id,this.form.value).subscribe(res => {
-    console.log('Categorie update successfully!');
-    this.router.navigateByUrl('/categories');
-  })
-  }
-
-  private getCategorie() {
-    let context = this;
-    context.categorieService.getCategorie(context.id).subscribe((productdata: any) => {
-      context.categorieDetails = productdata.data;
-      console.log(context.categorieDetails);
+    this.categorieService.updateCategorie(this.categoryId, this.form.value).subscribe(res => {
+      console.log('Categorie update successfully!');
+      this.router.navigateByUrl('/categories');
     });
   }
+
+  
 
 
 }
@@ -127,92 +155,51 @@ private updateCategorie(id: string, categorie: Categorie){
 
 
 
- /* @Input() name: string | undefined;
-
-  public mode: string | undefined;
-  private categorieIdFromRoute: any;
-  public categorieDetails: Categorie = new Categorie();
-  public pageTitle: string = '';
-
-
-  constructor(
-    private categorieService: CategorieService,
-    private router: Router,
-    private _activatedRoute: ActivatedRoute
-  ) {
-    let context = this;
-    context._activatedRoute.data.subscribe(param => {
-      if (typeof param.mode != 'undefined') {
-        context.mode = param.mode;
-      }
+ /*
+  *
+    this.form.valueChanges.subscribe(value => {
+      console.log(value);
     });
-    context.categorieIdFromRoute = this._activatedRoute.snapshot.paramMap.get('id');
+    this.form.get('content')?.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+  *  if (parameterMap.has('categoryId')) {
+    this.mode = 'EDIT';
 
+
+
+    this.isLoading = true;            //loading spinner initial
+    this.categorieService.getCategorie(this.categoryId).subscribe((categorieData:any) => {
+      this.isLoading = false;
+
+      this.categorie = {
+        id: categorieData.data._id,
+        title: categorieData.data.title,
+        content: categorieData.data.content
+       };   //_id comming from the db
+      this.form.setValue(
+        {
+         'title': this.categorie.title,
+         'content': this.categorie.content
+        });         // allow us to ovveride the value form your form control when we want to update
+    });
+  } else {
+    this.param = "ADD";
+    this.categoryId = null;
   }
-
-  ngOnInit(): void {
-    let context = this;
-   // context.categorieDetails.name = '';
-    switch (context.mode) {
-      case "ADD":
-        context.addCase();
-        break;
-      case "EDIT":
-        context.editCase();
-        break;
-      case "DETAIL":
-        context.viewCase();
-        break;
-    }
+  *
+  * public newCategorie = {
+    id: '',
+    title: '',
+    content: ''
   }
-
-  private getCategoryData() {
-    let context = this;
-    context.categorieService.viewCategorie(context.categorieIdFromRoute).subscribe(categoriedata => {
-      context.categorieDetails = categoriedata;
+  onSubmit(): void {
+    console.log('submitted: ', this.newCategorie);
+    this.categorieService.createCategorie(this.newCategorie).subscribe(added => {
+      console.log(added);
+      this.router.navigate(["/categories"]);
     });
   }
-
-  private viewCase() {
-    let context = this;
-    context.pageTitle = 'View categoria';
-    context.getCategoryData();
-  }
-
-  private addCase() {
-    let context = this;
-    context.pageTitle = 'Crea categoria';
-  }
-
-  private editCase() {
-    let context = this;
-    context.pageTitle = 'Modifica categoria';
-    context.getCategoryData();
-  }
-
-  public goBack() {
-    let context = this;
-    context.router.navigate(['/categories']);
-  }
-
-  public submitCategorie(): void {
-
-    let context = this;
-    let categorieObject = {
-      id: context.categorieDetails.id,
-      name: context.categorieDetails.name
-    };
-
-    if (context.mode == 'EDIT') {
-      context.categorieService.updateCategorie(context.categorieIdFromRoute, categorieObject).subscribe(data => {
-        context.router.navigateByUrl('/categories');
-      });
-    } else {
-      context.categorieService.createCategorie(categorieObject).subscribe(data => {
-        context.router.navigateByUrl('/categories');
-      });
-    }
-
   }*/
 
 
